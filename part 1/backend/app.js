af22cbc789d03b4679307517ef3452917d334b71
Emmannweb <emmanuelnoc@gmail.com>
2023-02-06 17:33:38 -0300

    [LESSON 1 INSTALL SERVER AND DB CONNECTION]

---
+++ part 1/backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+
+
+
+
+//database connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log(err));
+
+
+//port
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
